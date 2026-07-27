b36f62865815c6507e2d087f37cf072ade65c468
Ashim khan <116441770+Ashim74@users.noreply.github.com>
2026-07-28 00:08:33 +0530

    Fix Compose layout and sensor navigation compilation

--- app/src/main/java/com/droidnova/fliptomute/ui/navigation/FlipToMuteNavHost.kt
+++ app/src/main/java/com/droidnova/fliptomute/ui/navigation/FlipToMuteNavHost.kt
@@ -3,10 +3,10 @@ package com.droidnova.fliptomute.ui.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.NavHostController
 import com.droidnova.fliptomute.ui.screens.home.HomeRoute
 import com.droidnova.fliptomute.ui.screens.permissions.PermissionsScreen
 import com.droidnova.fliptomute.ui.screens.sensor_test.SensorTestScreen
@@ -30,7 +30,9 @@ fun FlipToMuteNavHost(modifier: Modifier = Modifier) {
         }
         composable(Destination.Permissions.route) { PermissionsScreen { navController.navigateUp() } }
         composable(Destination.Settings.route) { SettingsScreen { navController.navigateUp() } }
-        composable(Destination.SensorTest.route) { SensorTestScreen { navController.navigateUp() } }
+        composable(Destination.SensorTest.route) {
+            SensorTestScreen(onBack = { navController.navigateUp() })
+        }
     }
 }
 
