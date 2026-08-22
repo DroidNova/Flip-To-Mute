a5ebc05385a4e8318aa7280463b3e334c2265328
Ashim khan <116441770+Ashim74@users.noreply.github.com>
2026-08-22 21:45:06 +0530

    Use production collapsible banner ad unit

--- app/src/main/java/com/droidnova/fliptomute/ads/AdUnits.kt
+++ app/src/main/java/com/droidnova/fliptomute/ads/AdUnits.kt
@@ -1,6 +1,6 @@
 package com.droidnova.fliptomute.ads
 
-/** Google-provided test ad unit IDs used during development. */
+/** AdMob ad unit IDs used by Flip To Mute. */
 object AdUnits {
-    const val HOME_COLLAPSIBLE_BANNER = "ca-app-pub-3940256099942544/2014213617"
+    const val HOME_COLLAPSIBLE_BANNER = "ca-app-pub-4788231589271799/6127251574"
 }
