c39f014a829171cb2f98b28cd46039c292f97750
mantsha <mantsha@7379>
2026-08-26 21:01:38 +0530

    commit-release5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.droidnova.fliptomute"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     buildTypes {
